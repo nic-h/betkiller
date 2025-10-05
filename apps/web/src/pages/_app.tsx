2a31a0198ee7f62794400787f53a7d18712da012
nic-h <nic@nichamilton.info>
2025-10-06 09:26:13 +1100

    fix: restore styles and disable npm workspace flag for dev

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 
-import "@/styles/tokens.css";
-import "@/styles/globals.css";
+import "../../styles/tokens.css";
+import "../../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
